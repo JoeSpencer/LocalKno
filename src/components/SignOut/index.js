f690ad5fe8235d7b7680fd7671201e3923cf9e84
Joe Spencer <spencerjoe@mac.com>
2019-12-16 08:35:55 -0600

    first commit

---
+++ src/components/SignOut/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withFirebase } from '../Firebase';
+
+
+const SignOutButton = ({ firebase }) => (
+  <button class="inline-block text-sm px-4 py-2 leading-none border rounded-full text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0" type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
+);
+export default withFirebase(SignOutButton);
